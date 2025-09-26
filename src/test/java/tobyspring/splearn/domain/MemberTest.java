6c542c5c8b9e284ed52c2e570d509084085829d7
Seokju Hong <iamseokjuhong@gmail.com>
2025-09-26 19:19:59 +0900

    13. Member 엔티티 생성

---
+++ src/test/java/tobyspring/splearn/domain/MemberTest.java
@@ -0,0 +1,15 @@
+package tobyspring.splearn.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberTest {
+
+    @Test
+    void createMember() {
+        var member = new Member("toby@splearn.app", "Toby", "secret");
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+}
