91d1097bb95c7505a4539be97edc903e13ad77bb
somefood <iamseokjuhong@gmail.com>
2025-09-26 22:45:07 +0900

    15. 스프링을 이용한 널(null) 안정성 확보

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
+    id("com.github.spotbugs") version "6.1.11"
 }
 
 group = "tobyspring"
