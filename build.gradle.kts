7ff9baf04a91012078c7ff5efd17e82d8af21e8d
Seokju Hong <iamseokjuhong@gmail.com>
2025-09-30 13:15:34 +0900

    34. 엔티티의 equals()와 hashCode() 구현

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,3 +48,7 @@ tasks.withType<Test> {
     useJUnitPlatform()
     jvmArgs("-javaagent:${mockitoAgent.asPath}" )
 }
+
+spotbugs {
+    excludeFilter.set(file("${projectDir}/spotbugs-exclude-filter.xml"))
+}
